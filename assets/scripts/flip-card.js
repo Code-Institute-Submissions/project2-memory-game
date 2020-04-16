11d2210ab752067a586811575f881959fb803448
MilanStefanik <stefanik.milan@gmail.com>
2020-04-16 16:36:20 +0000

    basic memory game logic created

--- assets/scripts/flip-card.js
+++ assets/scripts/flip-card.js
@@ -1,6 +1,6 @@
 //flip card effect on click
-document.querySelectorAll(".flipCardContainer").forEach(item => {
-    item.addEventListener("click", function() {
-        item.classList.toggle("flip");
-    })
-})
+// document.querySelectorAll(".flipCardContainer").forEach(item => {
+//     item.addEventListener("click", function() {
+//         item.classList.toggle("flip");
+//     })
+// })
