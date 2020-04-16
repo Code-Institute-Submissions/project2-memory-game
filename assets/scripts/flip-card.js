edef6ab2d07d7891501481358ec13cbc2f3c3729
MilanStefanik <stefanik.milan@gmail.com>
2020-04-16 14:45:00 +0000

    Physical images replaced by Font Awesome Links Array, Card flip effect added.

---
+++ assets/scripts/flip-card.js
@@ -0,0 +1,6 @@
+//flip card effect on click
+document.querySelectorAll(".flipCardContainer").forEach(item => {
+    item.addEventListener("click", function() {
+        item.classList.toggle("flip");
+    })
+})
