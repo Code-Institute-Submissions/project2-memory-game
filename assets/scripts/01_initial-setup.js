b50d7336a7adedfefb3309d663dbc6b4ea89f159
MilanStefanik <stefanik.milan@gmail.com>
2020-05-09 18:33:13 +0000

    Jasmine test - fitScreen function

--- assets/scripts/01_initial-setup.js
+++ assets/scripts/01_initial-setup.js
@@ -1,19 +1,17 @@
 //adjust elements and font sizes on web page load
 function onLoad() {
-    fitScreen();
+    fitScreen($(window).width(),$(window).height());
     createIntroPage();
 }
 
 
 //adjust elements and font sizes on window resize
 window.addEventListener('resize', function () {
-    fitScreen();
+    fitScreen($(window).width(),$(window).height());
 })
 
-function fitScreen() {
-    let winWidth = $(window).width(); //setting window width variable
-    let winHeight = $(window).height(); //setting window height variable
-
+function fitScreen(winWidth, winHeight) {
+    
     square.style.width = Math.min(winWidth, (winHeight - 100)) / winWidth * 100 + '%'; //setting the size of square used as play-board to fit the screen, 100 represents header height
      
     jQuery('html').css('font-size', ((Math.min(winWidth, (winHeight - 100)) / winWidth * 2) + 'vw')); //setting the root font size to fit elements depending on the screen size
--- assets/specs/memory-game.spec.js
+++ assets/specs/memory-game.spec.js
@@ -7,8 +7,6 @@ describe("Memory Game", function () {
 
     describe("On Load Setup", function () {
 
-
-
         it("With onLoad() function, fitScreen() function should be called", function () {
             fitScreen = jasmine.createSpy();
             onLoad();
@@ -25,11 +23,24 @@ describe("Memory Game", function () {
 
     describe("fitScreen Function", function () {
 
-        it("Game area should adjust to smaller of screen width and screen height", function () {
-            spyOnProperty($(window), 'width').and.returnValue(1000);
-            spyOnProperty($(window), 'height').and.returnValue(500);
-            fitScreen();
-            expect(square.style.width).toBe('40%');
+        it("Game area and html root font size should adjust to smaller of screen width and screen height - smaller height", function () {
+            var winWidth = 1000;
+            var winHeight = 500;
+            fitScreen(winWidth, winHeight);
+            var result1 = square.style.width;
+            var result2 = jQuery('html').css('font-size');
+            expect(result1).toBe('40%');
+            expect(result2).toBe('25.6px');
+        });
+
+        it("Game area and html root document font size should adjust to smaller of screen width and screen height - smaller width", function () {
+            var winWidth = 500;
+            var winHeight = 1000;
+            fitScreen(winWidth, winHeight);
+            var result1 = square.style.width;
+            var result2 = jQuery('html').css('font-size');
+            expect(result1).toBe('100%');
+            expect(result2).toBe('64px');
         });
     });
 
